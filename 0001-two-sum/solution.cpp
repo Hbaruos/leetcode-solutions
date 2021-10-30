9d30e0d3f6afb25b6e4eb0ed10502490f97ed15e
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2021-10-30 08:50:59 +0000

    [LeetCode Sync] Runtime - 312 ms (5.04%), Memory - 10.2 MB (99.99%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,16 +1,12 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (m.find(target - nums[i]) != m.end()) {
-                res.push_back(m[target - nums[i]]);
-                res.push_back(i);
-                return res;
+    public:
+        std::vector<int> twoSum(std::vector<int> &nums, int target) {
+            for(int i=0; i<nums.size(); ++i) {
+                for(int j=i+1; j<nums.size(); ++j) {
+                    if(nums[i]+nums[j] == target)
+                        return {i,j};
+                }   
             }
-            m[nums[i]] = i;
+            return {}; 
         }
-        return res;
-    }
 };
