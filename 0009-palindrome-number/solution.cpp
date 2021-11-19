5c6c14de925e7da45c38475079041046a833feb3
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2021-11-19 04:41:33 +0000

    Sync LeetCode submission Runtime - 12 ms (5.82%), Memory - 5.9 MB (100.00%)

---
+++ 0009-palindrome-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        bool isPalindrome(int x) {
+            if(x<0) 
+                return false;
+            int temp = x;
+            double ans = 0;
+            while(x != 0) {
+                ans = (ans*10) + (x%10);
+                x /= 10;
+            } 
+            if(ans == temp) return true;
+            else return false;
+        }
+};
