e3a9ecc13086de1f6a452f13eff0de4a87ca4c0f
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-03-31 07:09:40 +0000

    Sync LeetCode submission Runtime - 33 ms (5.68%), Memory - 7.6 MB (100.00%)

---
+++ 0013-roman-to-integer/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m = {{'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000}};
+        int num = 0;
+        
+        for (int i = 0; i < s.size() - 1; ++i) {
+            if (m[s[i]] < m[s[i + 1]]) num -= m[s[i]];
+            else num += m[s[i]];
+        }
+        num += m[s.back()];
+        return num;
+    }
+};
