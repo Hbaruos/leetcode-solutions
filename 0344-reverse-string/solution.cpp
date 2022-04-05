5f627a3f329cddca97ad76695a5b5ba8315c3e49
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-04-05 07:05:10 +0000

    [LeetCode Sync] Runtime - 41 ms (2.04%), Memory - 23.2 MB (100.00%)

--- 0344-reverse-string/solution.cpp
+++ 0344-reverse-string/solution.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    void _reverseString(vector<char> &s, int i, int j) {
-        if (i < j) 
-            swap(s[i],s[j]) , _reverseString(s, i + 1, j - 1);
-        else return;
-    }
-    
     void reverseString(vector<char>& s) {
-        _reverseString(s, 0, s.size() - 1);    
+        reverse(s.begin(), s.end());
     }
 };
