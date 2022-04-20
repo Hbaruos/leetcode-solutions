69c1fad90ef6c3a159ac420ee2033c1ee27d6289
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-04-20 10:32:31 +0000

    Sync LeetCode submission Runtime - 29 ms (5.40%), Memory - 57.8 MB (99.95%)

--- 0099-recover-binary-search-tree/solution.cpp
+++ 0099-recover-binary-search-tree/solution.cpp
@@ -1,27 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    
-    void markTree(TreeNode* root, TreeNode*& prev, TreeNode*& first, TreeNode*& end) {
-        if (!root) return;
-        markTree(root->left, prev, first, end);
-        if (prev) {
-            if (root->val < prev->val) {
-                if (!first) {
-                    first = prev;
-                }
-                end = root;
-            }
-        }
-        prev = root;
-        markTree(root->right, prev, first, end);
-    }
-    
-    
 public:
     void recoverTree(TreeNode* root) {
-        TreeNode *prev = nullptr, *first = nullptr, *end = nullptr;
-        markTree(root, prev, first, end);
-        swap(first->val, end->val);
-        return;
+        TreeNode *prev(nullptr), *start(nullptr), *end(nullptr);
+        f(root, prev, start, end);
+        swap(start->val, end->val);
     }
+    
+    void f(TreeNode *root, TreeNode *&prev, TreeNode *&start, TreeNode *&end) {
+        if (!root) return;
+        f(root->left, prev, start, end);
+        if (prev && prev->val > root->val) { 
+            if (!start) start = prev;
+            end = root;
+        }
+        prev = root;
+        f(root->right, prev, start, end);
+    }
+    
 };
