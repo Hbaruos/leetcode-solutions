ec281e51db1625ad3bb07b16a367553ac150593d
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-04-21 11:08:07 +0000

    [LeetCode Sync] Runtime - 60 ms (5.18%), Memory - 13.2 MB (81.92%)

--- 1250-longest-common-subsequence/solution.cpp
+++ 1250-longest-common-subsequence/solution.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
-        vector<int> prev(m + 1, 0), cur(m + 1, 0);
-        
-        for (int j = 0; j <= m; ++j) prev[j] = 0;
-        
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                if (s1[i - 1] == s2[j - 1]) cur[j] = 1 + prev[j - 1];
-                else cur[j] = max(prev[j], cur[j - 1]);
-            }
-            prev = cur;
-        }
-        return prev[m];
+    int f(string &s1, string &s2, int i, int j, vector<vector<int>> &dp) {
+        if (i < 0 || j < 0) return 0;
+        if (s1[i] == s2[j]) return dp[i][j] = 1 + f(s1, s2, i - 1, j - 1, dp);
+        if (dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = 0 + max(f(s1, s2, i - 1, j, dp), f(s1, s2, i, j - 1, dp));
     }
+    
+    int longestCommonSubsequence(string s1, string s2) {
+        int i = s1.size(), j = s2.size();
+        vector<vector<int>> dp(i, vector<int> (j, -1));
+        return f(s1, s2, i - 1, j - 1, dp);
+        
+    }    
 };
