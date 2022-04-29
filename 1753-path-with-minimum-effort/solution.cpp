1fc40c07dc5f82627a023567677723cdde423886
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-04-29 15:42:49 +0000

    Sync LeetCode submission Runtime - 168 ms (16.46%), Memory - 20.2 MB (99.95%)

--- 1753-path-with-minimum-effort/solution.cpp
+++ 1753-path-with-minimum-effort/solution.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-int minimumEffortPath(vector<vector<int>>& h) {
-    int si = h.size(), sj = h[0].size();
-    vector<vector<int>> eff(si, vector<int>(sj, 1000000));
-    vector<int> dir{0, 1, 0, -1, 0};
-    auto comp = [&](const array<int, 3> &p1, const array<int, 3> &p2) { return p1[0] >= p2[0]; };
-    priority_queue<array<int, 3>, vector<array<int, 3>>, decltype(comp)> pq(comp);
-    eff[0][0] = 0;
-    pq.push({0, 0, 0});
-    while(!pq.empty()) {
-       auto [e, i, j] = pq.top();
-       pq.pop();
-       if (i == si - 1 && j == sj - 1)
-           break;
-       for (auto d = 0; d < 4; ++d) {
-           int x = i + dir[d], y = j + dir[d + 1];
-           if (x >= 0 && y >= 0 && x < si && y < sj) {
-               int new_eff = max(eff[i][j], abs(h[i][j] - h[x][y]));
-               if (new_eff < eff[x][y]) {
-                   eff[x][y] = new_eff;
-                   pq.push({new_eff, x, y});
-               }
-           }
+    int minimumEffortPath(vector<vector<int>>& h) {
+        int r = h.size(), c = h[0].size();
+        vector<vector<int>> eff(r, vector<int> (c, INT_MAX));
+        vector<int> dir{0, 1, 0, -1, 0};
+        auto comp = [&] (const array<int, 3> &p1, const array<int, 3> &p2) {return p1[0] >= p2[0];};
+        priority_queue<array<int, 3>, vector<array<int, 3>>, decltype(comp)> pq(comp);
+        eff[0][0] = 0;
+        pq.push({0, 0, 0});
+        while (!pq.empty()) {
+            auto [e, i, j] = pq.top();
+            pq.pop();
+            if (i == r - 1 && j == c - 1) break;
+            for (auto d = 0; d < 4; ++d) {
+                int x = i + dir[d], y = j + dir[d + 1];
+                if (x >= 0 && y >= 0 && x < r && y < c) {
+                    int new_eff = max(eff[i][j], abs(h[i][j] - h[x][y]));
+                    if (new_eff < eff[x][y]) {
+                        eff[x][y] = new_eff;
+                        pq.push({new_eff, x, y});
+                    }
+                }
+            }
         }
+        return eff[r - 1][c - 1];
     }
-    return eff[si - 1][sj - 1];
-}
 };
