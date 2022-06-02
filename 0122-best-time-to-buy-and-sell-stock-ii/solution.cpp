177d9fd66fbad80c8c7e58dd40744540ed80b86f
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-06-02 17:11:34 +0000

    Sync LeetCode submission Runtime - 3 ms (41.98%), Memory - 12.8 MB (100.00%)

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+        // vector<int> prev(2, 0);  aheadnotbuy, aheadbuy
+        // vector<int> cur(2, 0);   curnotbuy, curbuy
+        int aheadNotBuy, aheadBuy, curBuy, curNotBuy;
+        
+        
+        // prev[0] = prev[1] = 0;
+        aheadNotBuy = aheadBuy = 0;
+        
+        for (int i = n - 1; i >= 0; --i) {
+            
+            curBuy = max(-prices[i] + aheadNotBuy, 
+                                0 + aheadBuy);
+            
+            curNotBuy = max(prices[i] + aheadBuy,
+                                0 + aheadNotBuy);
+         
+            // prev = cur;
+            aheadBuy = curBuy;
+            aheadNotBuy = curNotBuy;
+        }
+        // return prev[1];
+        return aheadBuy;
+    }
+};
