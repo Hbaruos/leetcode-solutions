32b63974e3bb7053b24be723244a8161102d788c
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-06-12 03:44:27 +0000

    Sync LeetCode submission Runtime - 251 ms (5.08%), Memory - 75.3 MB (100.00%)

---
+++ 0084-largest-rectangle-in-histogram/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
+        
+        for (int i = 0; i <= n; ++i) {
+            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int height = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : i - st.top() - 1; 
+                maxArea = max(maxArea, width * height);
+            }     
+            st.emplace(i);
+        }
+        return maxArea;
+    }
+};
