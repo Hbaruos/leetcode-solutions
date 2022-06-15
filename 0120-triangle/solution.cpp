5dbdb8a2a0b2faa7dfd2084431a0b90fc8ababbd
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-06-15 14:02:06 +0000

    Sync LeetCode submission Runtime - 9 ms (3.28%), Memory - 8.6 MB (100.00%)

--- 0120-triangle/solution.cpp
+++ 0120-triangle/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& t) {
-        int n = t.size();
-        vector<int> dp(n);
+    int minimumTotal(vector<vector<int>>& triangle) 
+    {
+        auto rows = triangle.size();
+        if (rows == 0) return 0;
         
-        for (int j = 0; j < n; ++j)
-            dp[j] = t[n - 1][j];
-        
-        for (int i = n - 2; i >= 0; --i) {
-            vector<int> cur(n);
-            for (int j = i; j >= 0; --j) {
-                int dwn = t[i][j] + dp[j];
-                int diag = t[i][j] + dp[j + 1];
-                cur[j] = min(dwn, diag);
+        for (int i = rows - 2; i >= 0; --i)
+        {
+            for (int j = 0; j < triangle[i].size(); ++j)
+            {
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
             }
-            dp = cur;
         }
-        return dp[0];
+        
+        return triangle[0][0];
     }
 };
