fbdac7130929f425943df5494fbef690174f71d9
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-06-16 09:17:58 +0000

    Sync LeetCode submission Runtime - 479 ms (11.36%), Memory - 387.2 MB (5.02%)

--- 0005-longest-palindromic-substring/solution.cpp
+++ 0005-longest-palindromic-substring/solution.cpp
@@ -1,46 +1,27 @@
 class Solution {
-    public:
-        std::string longestPalindrome(std::string s) {
-            std::string x = "";
-            int l = 0;
-            x += s[0];
-            if(s.empty()) return "";
-            if(s.size() == 1) return s;
-
-            for(int i=0; i<s.size(); ++i) {
-                int i1 = i, i2 = i + 1;
-                while(i1 >= 0 && i2 < s.size()) {
-                    if(s[i1] == s[i2]) {
-                        int len = i2 - i1 + 1;
-                        if(len > l) {
-                            l = len;
-                            x = s.substr(i1, len);
-                        }
-                    }
-                    else 
-                        break;
-                    i1--;
-                    i2++;
-                    
-                }
-            }
+public:
+    string longestPalindrome(string s) {
+        vector<vector<int>> dp(size(s), vector<int> (size(s), 0));
+        int l = 0, r = 1;    
+        
+        for (int i = 0; i < size(s); ++i)
+            dp[i][i] = 1;
             
-            for(int i=0; i<s.size(); ++i) {
-                    int i1 = i - 1, i2 = i + 1;
-                    while(i1 >= 0 && i2 < s.size()) {
-                        if(s[i1] == s[i2]) {
-                            int len = i2 - i1 + 1;
-                            if(len > l) {
-                                l = len;
-                                x = s.substr(i1, len);
-                            }
-                        }
-                        else 
-                            break;
-                        i1--;
-                        i2++;
-                    }
+        for (int i = 0; i < size(s) - 1; ++i)
+            if (s[i] == s[i + 1])
+                dp[i][i + 1] = 1, l = i, r = 2;
+                
+        for (int j = 2; j < size(s); ++j) {
+            for (int i = 0; i < size(s) - j; ++i) {
+                
+                int left = i;
+                int right = i + j;
+                
+                if (s[left] == s[right] && dp[left + 1][right - 1]) {
+                    dp[left][right] = 1; l = i; r = j + 1;
+                }   
             }
-            return x;
         }
+        return s.substr(l, r);
+    }
 };
