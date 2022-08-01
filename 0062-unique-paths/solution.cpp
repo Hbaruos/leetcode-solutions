0f56f71ccc8cae6ff47e1d921405d98d7fdc0c00
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-08-01 07:06:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.5 MB (100.00%)

--- 0062-unique-paths/solution.cpp
+++ 0062-unique-paths/solution.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> prev(n, 0);
-    for (int i = 0; i < m; ++i) {
-        vector<int> cur(n, 0);
-        for (int j = 0; j < n; ++j) {
-            if (i == 0 && j == 0) cur[j] = 1;
-            else {
-                int up = 0;
-                int left = 0;
-                if (i > 0) up = prev[j];
-                if (j > 0) left = cur[j - 1];
-                cur[j] = up + left;
+        vector<vector<int>> dp(m, vector<int> (n, 1));
+        
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
             }
         }
-        prev = cur;
-    }
-    return prev[n - 1];
+        return dp[m - 1][n - 1];
     }
 };
