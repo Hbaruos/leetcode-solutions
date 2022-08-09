b36636e5886c4b0271c622c90860d067f719f56e
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2022-08-09 12:04:13 +0000

    [LeetCode Sync] Runtime - 94 ms (5.01%), Memory - 23.5 MB (100.00%)

--- 1402-count-square-submatrices-with-all-ones/solution.cpp
+++ 1402-count-square-submatrices-with-all-ones/solution.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    int countSquares(vector<vector<int>>& matrix) {
-        int sum = 0;
-        
-        for (int i = 1; i < matrix.size(); ++i)
-            for (int j = 1; j < matrix[0].size(); ++j)
-                if (matrix[i][j]) 
-                    matrix[i][j] = 1 + min(matrix[i - 1][j - 1], min(matrix[i - 1][j], matrix[i][j - 1]));
-        
-        
-        for (auto &it : matrix)
-            sum += accumulate(it.begin(), it.end(), 0);
-        
-        return sum;
+    int countSquares(vector<vector<int>>& A) {
+        int res = 0;
+        for (int i = 0; i < A.size(); ++i)
+            for (int j = 0; j < A[0].size(); res += A[i][j++])
+                if (A[i][j] && i && j)
+                    A[i][j] += min({A[i - 1][j - 1], A[i - 1][j], A[i][j - 1]});
+        return res;
     }
 };
