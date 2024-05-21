176b4a56e2053e720e5d034e7b137e55e68dc4c0
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2024-05-21 04:47:03 +0000

    Sync LeetCode submission Runtime - 150 ms (5.78%), Memory - 126.3 MB (99.97%)

---
+++ 2271-rearrange-array-elements-by-sign/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> result(nums.size());
+        int posIndex = 0, negIndex = 1;
+        for (auto num : nums) {
+            if (num > 0) {
+                result[posIndex] = num; 
+                posIndex += 2;
+            }
+            else {
+                result[negIndex] = num;
+                negIndex += 2;
+            }
+        }
+        return result;
+    }
+};
