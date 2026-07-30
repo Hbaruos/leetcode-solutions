74747d6c4855338fea77dfca56f26342039ad11f
Hbaruos <84589149+Hbaruos@users.noreply.github.com>
2026-07-30 18:04:36 +0000

    [LeetCode Sync] - Cache With Time Limit - Runtime - 55 ms (26.50%), Memory - 54.4 MB (77.06%)

--- 2762-cache-with-time-limit/solution.ts
+++ 2762-cache-with-time-limit/solution.ts
@@ -1,26 +1,25 @@
 class TimeLimitedCache {
-    cache = new Map<number, { value: number; timeout?: NodeJS.Timeout | undefined }>();
-    
-    set(key: number, value: number, duration: number): boolean {
-        const existingValue = this.cache.get(key);
+  cache = new Map<number, { value: number; timeout?: NodeJS.Timeout | undefined }>();
 
-        if (existingValue) {
-            clearTimeout(existingValue.timeout);
-        }
+  set(key: number, value: number, duration: number): boolean {
+    const existingValue = this.cache.get(key);
 
-        const timeout = setTimeout(() => this.cache.delete(key), duration);
-
-        this.cache.set(key, { value, timeout });
-        return !!existingValue;
+    if (existingValue) {
+      clearTimeout(existingValue.timeout);
     }
 
-    get(key: number): number {
-        const entry = this.cache.get(key);
-        return entry ? entry.value : -1;
-    }
+    const timeout = setTimeout(() => this.cache.delete(key), duration);
 
-    count(): number {
-        return this.cache.size;
-    }
+    this.cache.set(key, { value, timeout });
+    return !!existingValue;
+  }
+
+  get(key: number): number {
+    const entry = this.cache.get(key);
+    return entry ? entry.value : -1;
+  }
+
+  count(): number {
+    return this.cache.size;
+  }
 }
-
